5d2bc7ba251ae45e4c52a8e48715fb18e28aee71
Sriram0009 <Chamarthi.v@northeastern.edu>
2025-06-11 21:48:15 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def main():
+    # Title and tag line with styling
+    st.title(" 🌟 Personal Wardrobe Stylist 🌟 ")
+    st.write("Let Your Event Speak Your Style! 💃🕺")
+
+    # Overview text with emphasis on keywords
+
+    st.header("👕 Your Style, Your Way")
+    st.write("Embarking on a revolutionary journey in fashion technology, our Personal Wardrobe Stylist Application redefines how customers explore and embrace style. Seamlessly blending cutting-edge AI technology with an extensive product inventory, our mission is to simplify the daunting task of selecting the perfect outfit for every occasion. The interface serves as a gateway to this transformative experience, offering an intuitive platform where users effortlessly input event details, initiating a personalized fashion voyage. Through Streamlit's streamlined interface, users engage in a sophisticated yet user-friendly space, triggering AI-powered algorithms to craft bespoke fashion recommendations. This amalgamation of technology promises a refined, convenient, and engaging fashion exploration for our users.")
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# FashIQ
-Designed an AI-powered wardrobe assistant that recommends outfits using NLP and computer vision. Integrated GPT-4 and CLIP to interpret style prompts and match apparel. Enabled real-time fashion suggestions via FastAPI and product scraping for personalized, image-aware clothing recommendations.
+# FashIQ - Personal Wardrobe Stylist README 🚀
+
+
+
+### Streamlit App 🌟
+
+#### 1. How to Use the Streamlit App locally:
+1. Clone the repository to your local machine:
+
+
+  ```bash
+   git clone https://github.com/Vicky1240/FashIQ.git
+   ```
+
+2. Create environment
+```
+conda create --name stenv python==3.9.0
+conda activate stenv
+   ```
+
+3. Install Requirements
+```
+pip install -r requirements.txt
+
+   ```
+  4. Run the app
+
+   ```
+   streamlit run Main.py
+
+   ```
+
+- 📊 Users can give the query and get the recommended images from previously Wardrobe images and recommendation links to buy products from Macy's Online store
+
